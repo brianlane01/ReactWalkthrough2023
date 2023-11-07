ef6c086804a09faaef14aad8c65e4c6f7f85646a
brianlane01 <brian.storm.lane@icloud.com>
2023-11-07 11:35:02 -0500

    Updated the App.css

--- src/Components/Site/Home.js
+++ src/Components/Site/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return(
         <div className='main'>
             <div className='mainDiv'>
-                <h1>Welcome to RFeact Fundamentals at Eleven Fifty</h1>
+                <h1>Welcome to React Fundamentals at Eleven Fifty</h1>
                 <p>This app is intended to be a starter/tutorial app for your upcoming project and a Learning sandbox for you 
                     in months and years to come should you continue to develop software with React. In this application, we will 
                     work through some of the basic React concepts, build out some small applications, and then unleash you to 
